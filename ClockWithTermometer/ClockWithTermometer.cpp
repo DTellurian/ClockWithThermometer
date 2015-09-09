22b01a231f5feb93735ab7ac9849e26b3eceedfd
DTellurian <d.mihalych@gmail.com>
2015-09-09 10:45:25 +0300

    CreatingProject

---
+++ ClockWithTermometer/ClockWithTermometer.cpp
@@ -0,0 +1,17 @@
+/*
+ * ClockWithTermometer.cpp
+ *
+ * Created: 09.09.2015 10:44:39
+ *  Author: Mihalych
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
